8c4b0eef7f5bd99bd15dfd5bf0c2d8f3fbc4cc6a
Sahillanjewar <156170408+Sahillanjewar@users.noreply.github.com>
2026-02-10 09:36:28 +0530

    Create 12_easy_facebook_app_clickthrough_rate_ctr_.sql

---
+++ 12_easy_facebook_app_clickthrough_rate_ctr_.sql
@@ -0,0 +1,14 @@
+SELECT
+    app_id,
+    ROUND(
+        100 *
+        SUM(CASE WHEN event_type = 'click' THEN 1.0 ELSE 0.0 END) /
+        SUM(CASE WHEN event_type = 'impression' THEN 1.0 ELSE 0.0 END),
+        2
+    ) AS ctr
+FROM
+    events
+WHERE
+    DATE_PART('year', timestamp) = 2022
+GROUP BY
+    app_id;
