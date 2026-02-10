23f74c4ee31b03029a4a9507f71d9060aa91650a
Sahillanjewar <156170408+Sahillanjewar@users.noreply.github.com>
2026-02-10 09:42:08 +0530

    Create 21_easy_amazon_average_review_ratings.sql

---
+++ 21_easy_amazon_average_review_ratings.sql
@@ -0,0 +1,12 @@
+SELECT
+    DATE_PART('month', submit_date) AS mth,
+    product_id AS product,
+    ROUND(AVG(stars), 2) AS avg_stars
+FROM
+    reviews
+GROUP BY
+    DATE_PART('month', submit_date),
+    product_id
+ORDER BY
+    mth ASC,
+    product ASC;
