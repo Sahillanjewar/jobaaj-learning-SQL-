a74b7564a730d69be174cf1427a73df37ee3a227
Sahillanjewar <156170408+Sahillanjewar@users.noreply.github.com>
2026-02-10 09:30:29 +0530

    Create 04_easy_amazon_purchasing_activity_by_product_type.sql

---
+++ 04_easy_amazon_purchasing_activity_by_product_type.sql
@@ -0,0 +1,14 @@
+SELECT
+    t1.order_date,
+    t1.product_type,
+    (
+        SELECT
+            SUM(quantity)
+        FROM
+            total_trans AS t2
+        WHERE
+            t2.order_date <= t1.order_date AND
+            t2.product_type = t1.product_type
+    ) AS cum_purchased
+FROM
+    total_trans AS t1;
